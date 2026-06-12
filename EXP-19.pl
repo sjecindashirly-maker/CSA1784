527dc02b6dfa296b2d6b70495ded7790251f53ed
sjecindashirly-maker <sjecindashirly@gmail.com>
2026-06-12 09:20:02 +0530

    Add files via upload

---
+++ EXP-19.pl
@@ -0,0 +1,6 @@
+% Student - Teacher - Subject - Code Database
+
+student(john, smith, ai, cs101).
+student(mary, david, dbms, cs102).
+student(kevin, james, os, cs103).
+student(susan, robert, cn, cs104).
