64a403fad390259673c0ce9251487dd568d78e51
sjecindashirly-maker <sjecindashirly@gmail.com>
2026-06-12 09:31:41 +0530

    Add files via upload

---
+++ EXP-20.pl
@@ -0,0 +1,10 @@
+% Planets Database
+
+planet(mercury, inner, 88).
+planet(venus, inner, 225).
+planet(earth, inner, 365).
+planet(mars, inner, 687).
+planet(jupiter, outer, 4333).
+planet(saturn, outer, 10759).
+planet(uranus, outer, 30687).
+planet(neptune, outer, 60190).
