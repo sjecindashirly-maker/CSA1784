627738d19e8bcb64696061b51c3e06c28429600f
sjecindashirly-maker <sjecindashirly@gmail.com>
2026-06-15 20:43:51 +0530

    Add files via upload

---
+++ EXP-25.pl
@@ -0,0 +1,8 @@
+% Monkey Banana Problem
+
+can_get_banana :-
+    write('Monkey goes to the box'), nl,
+    write('Monkey pushes the box under the banana'), nl,
+    write('Monkey climbs onto the box'), nl,
+    write('Monkey reaches the banana'), nl,
+    write('Monkey grabs the banana'), nl.
